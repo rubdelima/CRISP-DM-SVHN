0a01e12bb20f64c6c96a22bab03cfd691e456392
Rubens Lima <rubensndelima@hotmail.com>
2025-08-03 22:35:40 -0300

    run all models

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -1,10 +1,10 @@
 import os
 import joblib
 
-def save_checkpoint(obj, name):
+def save_checkpoint(name, object):
     os.makedirs("checkpoints", exist_ok=True)
     path = os.path.join("checkpoints", f"{name}.pkl")
-    joblib.dump(obj, path)
+    joblib.dump(object, path)
 
 def load_checkpoint(name):
     path = os.path.join("checkpoints", f"{name}.pkl")
